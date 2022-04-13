fb0fcf49732d662510142e1a0e9b50cfa4ddf8d2
shpr <roman.shpr@gmail.com>
2022-04-13 21:45:27 +0300

    Add todo item factory

---
+++ src/todoItem.js
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+export function createTodoItem({
+	category = 'default',
+	title,
+	descr = '',
+	priority = 'low',
+	dueDate = 'n/a',
+} = {}) {
+	const id = uuidv4();
+	const prioritiesList = ['low', 'med', 'high'];
+	const completed = false;
+	if (!prioritiesList.includes(priority)) {
+		priority = 'low';
+		console.warn('Priority is not low/med/high. Defaulted to low.');
+	}
+
+	if (!(dueDate == 'n/a' || dueDate instanceof Date)) {
+		dueDate = 'n/a';
+		console.warn(
+			'dueDate must be instanse of Date or unspecified, defaulted to uspecified.'
+		);
+	}
+
+	return { id, category, title, descr, priority, dueDate, completed };
+}
