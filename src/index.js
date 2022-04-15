cf4f856491d7b62efba0284ac33dab45a2ca6844
shpr <roman.shpr@gmail.com>
2022-04-15 16:06:09 +0300

    Add sample data for default display and debugging purposes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
-import { createTodoItem } from './script/todoItem';
-import { pubsubAdapter } from './script/pubsubAdapter';
+// import { createTodoItem } from './script/todoItem';
+// import { pubsubAdapter } from './script/pubsubAdapter';
 import { todoList } from './script/todoList';
+import sampledata from './script/sampledata';
+sampledata();
 
-let item = createTodoItem({
-	title: 'Title1',
-	category: 'Cat1',
-	descr: 'a simple task',
-	priority: 'low',
-	dueDate: new Date(1995, 11, 17),
-});
-pubsubAdapter.publishNewItem(item);
-
-item = createTodoItem({
-	title: 'Title2',
-	category: 'Cat3',
-	descr: 'a simple111 task',
-	priority: 'med',
-	dueDate: new Date(1925, 11, 17),
-});
-pubsubAdapter.publishNewItem(item);
-
-item = createTodoItem({
-	title: 'Title4',
-	category: 'Cat5',
-	descr: 'a 111 task',
-	priority: 'med',
-	dueDate: new Date(2125, 11, 17),
-});
-pubsubAdapter.publishNewItem(item);
-pubsubAdapter.publishUpdateItem(item.id, { descr: 'UPDATED!!!' });
+setTimeout(() => {
+	console.log(todoList.getList());
+}, 200);
---
+++ src/script/sampledata.js
@@ -0,0 +1,204 @@
+import { pubsubAdapter } from './pubsubAdapter';
+import { createTodoItem } from './todoItem';
+import { addDays } from 'date-fns';
+export default () => {
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Clean dishes',
+			category: 'Home Tasks',
+			descr: 'Dirty plates are right there',
+			priority: 'high',
+			dueDate: new Date(),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Clean floors',
+			category: 'Home Tasks',
+			descr: '',
+			priority: 'low',
+			dueDate: addDays(new Date(), 1),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Do laundry',
+			category: 'Home Tasks',
+			descr: 'Its full',
+			priority: 'med',
+			dueDate: addDays(new Date(), 2),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Clean dishes',
+			category: 'Home Tasks',
+			descr: 'Dirty plates are right there',
+			priority: 'high',
+			dueDate: addDays(new Date(), 1),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Clean windows',
+			category: 'Home Tasks',
+			descr: '',
+			priority: 'high',
+			dueDate: addDays(new Date(), 7),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Move table',
+			category: 'Home Tasks',
+			descr: 'Blocks the garden window',
+			priority: 'low',
+			dueDate: addDays(new Date(), 3),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Fix kettle',
+			category: 'Home Tasks',
+			descr: '...or buy new one',
+			priority: 'high',
+			dueDate: addDays(new Date(), 2),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Buy toilet paper',
+			category: 'Home Tasks',
+			descr: 'Next store visit',
+			priority: 'high',
+			dueDate: addDays(new Date(), 15),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Read about module patterns',
+			category: 'Studies',
+			descr: '',
+			priority: 'med',
+			dueDate: addDays(new Date(), 2),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Write GUI for to-do"s project',
+			category: 'Studies',
+			descr: '',
+			priority: 'med',
+			dueDate: addDays(new Date(), 3),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Look for optimisation modules in js',
+			category: 'Studies',
+			descr: 'Liner and non-linear',
+			priority: 'med',
+			dueDate: addDays(new Date(), 8),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Finish progress through TOP curriculum',
+			category: 'Studies',
+			descr: '',
+			priority: 'low',
+			dueDate: addDays(new Date(), 60),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Setup elliptical',
+			category: 'Sport',
+			descr: 'Its not used where it stands now',
+			priority: 'high',
+			dueDate: addDays(new Date(), 1),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Bike ride 20km',
+			category: 'Sport',
+			descr: 'Meet up with guys @ shore',
+			priority: 'med',
+			dueDate: addDays(new Date(), 3),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Evening walk',
+			category: 'Sport',
+			descr: '',
+			priority: 'low',
+			dueDate: addDays(new Date(), 1),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Evening walk',
+			category: 'Sport',
+			descr: '',
+			priority: 'low',
+			dueDate: addDays(new Date(), 2),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Check weather for desert hike',
+			category: 'Sport',
+			descr: '',
+			priority: 'low',
+			dueDate: addDays(new Date(), 17),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Check-in at family doctor',
+			category: 'Health',
+			descr: '',
+			priority: 'low',
+			dueDate: addDays(new Date(), 27),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Get blood checks',
+			category: 'Health',
+			descr: '',
+			priority: 'low',
+			dueDate: addDays(new Date(), 57),
+		})
+	);
+
+	pubsubAdapter.publishNewItem(
+		createTodoItem({
+			title: 'Prep mother`s medical docs',
+			category: 'Health',
+			descr: '',
+			priority: 'high',
+			dueDate: addDays(new Date(), 2),
+		})
+	);
+};
