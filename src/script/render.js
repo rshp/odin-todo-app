0815f4fcc066e5c5666e1a9209d266f7ed4b6494
shpr <roman.shpr@gmail.com>
2022-04-16 23:18:41 +0300

    Refactor sorting by time

--- src/script/render.js
+++ src/script/render.js
@@ -1,80 +1,21 @@
 import TodoCard from './todoCard';
-import endOfToday from 'date-fns/endOfToday';
-import isBefore from 'date-fns/isBefore';
-import isAfter from 'date-fns/isAfter';
-import isFuture from 'date-fns/isFuture';
-import isPast from 'date-fns/isPast';
-import endOfWeek from 'date-fns/endOfWeek';
-import isValid from 'date-fns/isValid';
+import { sortIntoTimeCategories } from './sortIntoTimeCategories';
 export const render = (() => {
 	const appContainer = document.createElement('main');
 	appContainer.classList.add('app-container');
 	document.body.appendChild(appContainer);
-
-	/*----------Organize by Time--------------------*/
-	const tasksByTimeContainer = document.createElement('div');
-	tasksByTimeContainer.classList.add('tasks-by-time');
-	appContainer.appendChild(tasksByTimeContainer);
-
-	const timeCategories = [
-		{
-			displayName: 'Today',
-			filter: (date) => {
-				if (!isValid(date)) return false;
-				return isFuture(date) && isBefore(date, endOfToday());
-			},
-		},
-		{
-			displayName: 'This Week',
-			filter: (date) => {
-				if (!isValid(date)) return false;
-				return isFuture(date) && isBefore(date, endOfWeek(new Date()));
-			},
-		},
-		{
-			displayName: 'Later',
-			filter: (date) => {
-				if (!isValid(date)) return false;
-				return isAfter(date, endOfWeek(new Date()));
-			},
-		},
-		{
-			displayName: 'Overdue',
-			filter: (date) => {
-				if (!isValid(date)) return false;
-				return isPast(date);
-			},
-		},
-		{
-			displayName: 'No date',
-			filter: (date) => {
-				return !isValid(date);
-			},
-		},
-	];
-
-	timeCategories.forEach((category, index) => {
-		const catDiv = document.createElement('div');
-		catDiv.classList.add('category-container');
-		timeCategories[index].catDiv = catDiv;
-		const catTitle = document.createElement('p');
-		catTitle.textContent = category.displayName;
-		catTitle.classList.add('category-title');
-		catDiv.appendChild(catTitle);
-		tasksByTimeContainer.appendChild(catDiv);
-	});
 
 	let taskCategories = [];
 	const tasksByCategoriesContainer = document.createElement('div');
 	tasksByCategoriesContainer.classList.add('tasks-by-category');
 	appContainer.appendChild(tasksByCategoriesContainer);
 
+	sortIntoTimeCategories.setAppContainer(appContainer);
+	sortIntoTimeCategories.setupCategories();
+
 	function newItem(item) {
-		timeCategories.forEach((category) => {
-			const cardDiv = new TodoCard(item);
-			if (category.filter(item.dueDate))
-				category.catDiv.appendChild(cardDiv);
-		});
+		sortIntoTimeCategories.sortItem(item);
+
 		if (
 			!taskCategories.some((element) => {
 				return element.displayName == item.category;
---
+++ src/script/sortIntoTimeCategories.js
@@ -0,0 +1,79 @@
+import endOfToday from 'date-fns/endOfToday';
+import isBefore from 'date-fns/isBefore';
+import isAfter from 'date-fns/isAfter';
+import isFuture from 'date-fns/isFuture';
+import isPast from 'date-fns/isPast';
+import endOfWeek from 'date-fns/endOfWeek';
+import isValid from 'date-fns/isValid';
+import TodoCard from './todoCard';
+export const sortIntoTimeCategories = (() => {
+	let appContainer = null;
+
+	function setAppContainer(container) {
+		appContainer = container;
+	}
+
+	const timeCategories = [
+		{
+			displayName: 'Today',
+			filter: (date) => {
+				if (!isValid(date)) return false;
+				return isFuture(date) && isBefore(date, endOfToday());
+			},
+		},
+		{
+			displayName: 'This Week',
+			filter: (date) => {
+				if (!isValid(date)) return false;
+				return isFuture(date) && isBefore(date, endOfWeek(new Date()));
+			},
+		},
+		{
+			displayName: 'Later',
+			filter: (date) => {
+				if (!isValid(date)) return false;
+				return isAfter(date, endOfWeek(new Date()));
+			},
+		},
+		{
+			displayName: 'Overdue',
+			filter: (date) => {
+				if (!isValid(date)) return false;
+				return isPast(date);
+			},
+		},
+		{
+			displayName: 'No date',
+			filter: (date) => {
+				return !isValid(date);
+			},
+		},
+	];
+
+	function sortItem(item) {
+		timeCategories.forEach((category) => {
+			const cardDiv = new TodoCard(item);
+			if (category.filter(item.dueDate))
+				category.catDiv.appendChild(cardDiv);
+		});
+	}
+
+	function setupCategories() {
+		const tasksByTimeContainer = document.createElement('div');
+		tasksByTimeContainer.classList.add('tasks-by-time');
+		appContainer.appendChild(tasksByTimeContainer);
+
+		timeCategories.forEach((category, index) => {
+			const catDiv = document.createElement('div');
+			catDiv.classList.add('category-container');
+			timeCategories[index].catDiv = catDiv;
+			const catTitle = document.createElement('p');
+			catTitle.textContent = category.displayName;
+			catTitle.classList.add('category-title');
+			catDiv.appendChild(catTitle);
+			tasksByTimeContainer.appendChild(catDiv);
+		});
+	}
+
+	return { sortItem, setAppContainer, setupCategories };
+})();
