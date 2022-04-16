e0284692493ddf879b5c5c44649d8610e0bdf6b3
shpr <roman.shpr@gmail.com>
2022-04-16 20:58:22 +0300

    Add sorting into categories

--- src/script/render.js
+++ src/script/render.js
@@ -7,6 +7,15 @@ import isPast from 'date-fns/isPast';
 import endOfWeek from 'date-fns/endOfWeek';
 import isValid from 'date-fns/isValid';
 export const render = (() => {
+	const appContainer = document.createElement('main');
+	appContainer.classList.add('app-container');
+	document.body.appendChild(appContainer);
+
+	/*----------Organize by Time--------------------*/
+	const tasksByTimeContainer = document.createElement('div');
+	tasksByTimeContainer.classList.add('tasks-by-time');
+	appContainer.appendChild(tasksByTimeContainer);
+
 	const timeCategories = [
 		{
 			displayName: 'Today',
@@ -44,19 +53,6 @@ export const render = (() => {
 		},
 	];
 
-	let taskCategories = [];
-
-	const appContainer = document.createElement('main');
-	appContainer.classList.add('app-container');
-	document.body.appendChild(appContainer);
-
-	const tasksByCategoriesContainer = document.createElement('div');
-	tasksByCategoriesContainer.classList.add('tasks-section');
-	const tasksByTimeContainer = document.createElement('div');
-	tasksByTimeContainer.classList.add('tasks-section');
-	appContainer.appendChild(tasksByTimeContainer);
-	appContainer.appendChild(tasksByCategoriesContainer);
-
 	timeCategories.forEach((category, index) => {
 		const catDiv = document.createElement('div');
 		catDiv.classList.add('category-container');
@@ -65,9 +61,13 @@ export const render = (() => {
 		catTitle.textContent = category.displayName;
 		catTitle.classList.add('category-title');
 		catDiv.appendChild(catTitle);
-		tasksByCategoriesContainer.appendChild(catDiv);
+		tasksByTimeContainer.appendChild(catDiv);
 	});
-	console.log(timeCategories);
+
+	let taskCategories = [];
+	const tasksByCategoriesContainer = document.createElement('div');
+	tasksByCategoriesContainer.classList.add('tasks-by-category');
+	appContainer.appendChild(tasksByCategoriesContainer);
 
 	function newItem(item) {
 		timeCategories.forEach((category) => {
@@ -75,6 +75,28 @@ export const render = (() => {
 			if (category.filter(item.dueDate))
 				category.catDiv.appendChild(cardDiv);
 		});
+		if (
+			!taskCategories.some((element) => {
+				return element.displayName == item.category;
+			})
+		) {
+			const catDiv = document.createElement('div');
+			catDiv.classList.add('category-container');
+			catDiv.dataset.category = item.category;
+			const catTitle = document.createElement('p');
+			catTitle.textContent = item.category;
+			catTitle.classList.add('category-title');
+			catDiv.appendChild(catTitle);
+			taskCategories.push({ displayName: item.category, catDiv: catDiv });
+			tasksByCategoriesContainer.appendChild(catDiv);
+			const cardDiv = new TodoCard(item);
+			catDiv.appendChild(cardDiv);
+		} else {
+			const cardDiv = new TodoCard(item);
+			document
+				.querySelector(`[data-category='${item.category}']`)
+				.appendChild(cardDiv);
+		}
 	}
 
 	function deleteItem(itemId) {
