1e75a136eec0f4a4377e94ad0498bcd69fe84f07
shpr <roman.shpr@gmail.com>
2022-04-22 23:59:37 +0300

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'the-new-css-reset/css/reset.css';
 import './styles/index.css';
-import { todoList } from './script/todoList';
+// import { todoList } from './script/todoList';
 import sampledata from './script/sampledata';
 import render from './script/render';
-import { cardModal } from './script/cardModal';
+// import { cardModal } from './script/cardModal';
 sampledata();
