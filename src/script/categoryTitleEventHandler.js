bf05b801027ebd0ab99943c62e402e78b4198152
shpr <roman.shpr@gmail.com>
2022-04-22 16:47:17 +0300

    Add category collapse animation

--- src/script/categoryTitleEventHandler.js
+++ src/script/categoryTitleEventHandler.js
@@ -1,6 +1,12 @@
 export function categoryTitleEventHandler(e) {
+	e.target.classList.toggle('collapsed-category');
+	e.target.parentElement.classList.toggle('collapsed-category');
+	const isCollapsed = e.target.classList.contains('collapsed-category');
+	console.log(isCollapsed);
 	const categoryCards = e.target.parentElement.querySelectorAll('.todo-card');
 	categoryCards.forEach((card) => {
-		card.style.display = card.style.display === 'none' ? '' : 'none';
+		// card.style.display = card.style.display === 'none' ? '' : 'none';
+		if (isCollapsed) card.classList.add('collapsed-card');
+		else card.classList.remove('collapsed-card');
 	});
 }
